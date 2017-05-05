3c9eb079a263699b4ad7b3894787350cde69efae
LeRoyce Pearson <minergeemili@gmail.com>
2017-05-05 08:57:51 -0500

    Add in texture rendering

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 extern crate glium;
 extern crate termion;
 extern crate vecmath as vm;
+extern crate image;
 
 use termion::screen::AlternateScreen;
 use termion::event::Key;
@@ -44,19 +45,27 @@ const TURN_SPEED: f64 = 0.03;
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    tex_coords: [f32; 2],
 }
 
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, tex_coords);
 
 fn main() {
     use glium::DisplayBuild;
     let gl_request = glium::glutin::GlRequest::Specific(glium::glutin::Api::OpenGl, (2,1));
     let display = glium::glutin::WindowBuilder::new().with_gl(gl_request).build_glium().unwrap();
 
-    let vertex1 = Vertex { position: [-1.0, -1.0] };
-    let vertex2 = Vertex { position: [1.0, -1.0] };
-    let vertex3 = Vertex { position: [-1.0, 1.0] };
-    let vertex4 = Vertex { position: [1.0, 1.0] };
+    use std::io::Cursor;
+    let image = image::load(Cursor::new(&include_bytes!("../assets/Potash_10x10.png")[..]),
+                           image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+
+    let vertex1 = Vertex { position: [-1.0, -1.0], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [1.0, -1.0], tex_coords: [1.0, 0.0] };
+    let vertex3 = Vertex { position: [-1.0, 1.0], tex_coords: [0.0, 1.0] };
+    let vertex4 = Vertex { position: [1.0, 1.0], tex_coords: [1.0, 1.0] };
     let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex2, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
@@ -66,8 +75,12 @@ fn main() {
         #version 120
 
         attribute vec2 position;
+        attribute vec2 tex_coords;
+
+        varying vec2 v_tex_coords;
 
         void main() {
+            v_tex_coords = tex_coords;
             gl_Position = vec4(position, 0.0, 1.0);
         }
     "#;
@@ -75,21 +88,28 @@ fn main() {
     let fragment_shader_src = r#"
         #version 120
 
+        varying vec2 v_tex_coords;
+
+        uniform sampler2D tex;
+
         void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            gl_FragColor = texture2D(tex, v_tex_coords);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     loop {
-        for ev in display.poll_events() {
-            use glium::Surface;
-            let mut target = display.draw();
-            target.clear_color(0.0, 0.0, 1.0, 1.0);
-            target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-            target.finish().unwrap();
+        use glium::Surface;
+        let mut target = display.draw();
+        let uniforms = uniform! {
+            tex: &texture,
+        };
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
 
+        for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed |
                 glium::glutin::Event::ReceivedCharacter('q') => return,
